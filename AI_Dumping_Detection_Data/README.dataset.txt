f9d2a9ac5eb7f763cea440a024cad57ba59ed354
Rawinder Singh <72056829+lilwindax@users.noreply.github.com>
2023-09-16 22:26:11 +1200

    Initial Commit

---
+++ AI_Dumping_Detection_Data/README.dataset.txt
@@ -0,0 +1,6 @@
+# AI Dumping Detection > 2023-08-19 8:38pm
+https://universe.roboflow.com/aidumpingdetection/ai-dumping-detection
+
+Provided by a Roboflow user
+License: MIT
+
---
+++ main.py
@@ -0,0 +1,11 @@
+import ultralytics
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+
+# Use the model
+model.train(data="AI_Dumping_Detection_Data/data.yaml", epochs=3)  # train the model
+metrics = model.val()  # evaluate model performance on the validation set
+results = model("https://ultralytics.com/images/")  # predict on an image
+path = model.export(format="onnx")  # export the model to ONNX format
